3a05a7aed5cf4e97c3c3deb3f4959388a2659009
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-26 00:09:34 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,20 @@
+<<<<<<< HEAD
 def my_each (array)
   i = 0
   while i < array.length
     yield array[i]
     i += 1
   end
+=======
+def my_each (collection)# put argument(s) here
+  # code here
+  counter = 0 
+  while collection.empty? == false 
+    if counter == 0
+      counter++
+      return collection[0]
+    else
+      return collection[counter]
+  end
+>>>>>>> cefd2cb18bf392a9da5939ec2c52a51cca24cb9d
 end
